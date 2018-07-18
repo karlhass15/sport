317253e82559a54fc3b762500546f9c1d5271ab6
justin <justin@wagento.com>
2018-07-18 10:07:22 -0500

    Fixed registration

--- app/design/frontend/Wagento/sport/registration.php
+++ app/design/frontend/Wagento/sport/registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Magento/sport',
+    'frontend/Wagento/sport',
     __DIR__
 );
